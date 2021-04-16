76996f0c096b4624a8961ad5f2c12d0455891c55
Sarabraj Singh <sarsingh@redhat.com>
2021-04-15 22:39:59 -0400

    fixed database initialization errors

--- Scribr/src/main/java/ca/myconcordia/comp5541/scribr/db/DatabaseConnection.java
+++ Scribr/src/main/java/ca/myconcordia/comp5541/scribr/db/DatabaseConnection.java
@@ -4,13 +4,17 @@
 package ca.myconcordia.comp5541.scribr.db;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,12 +22,12 @@ import java.sql.Statement;
  */
 public class DatabaseConnection {
     private static Connection connection = null;
-    private static String url = "jdbc:sqlite:data.db";
-    private static final String REL_INITIALIZIE_DB_SCRIPT = "src/main/resources/initializeDb.sql";
+    private static final String URL = "jdbc:sqlite:data.db";
+    private static final String REL_INITIALIZIE_DB_SCRIPT = "initializeDb.sql";
     static {
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(DatabaseConnection.URL);
         } catch (SQLException ex) {
             ex.printStackTrace();
             System.err.println("could not establish connection to the database");
@@ -46,21 +50,25 @@ public class DatabaseConnection {
         String s = new String();
         String[] commands = null;
 
-        try (FileReader fr = new FileReader(new File(DatabaseConnection.REL_INITIALIZIE_DB_SCRIPT));
-                BufferedReader br = new BufferedReader(fr)) {
-            while ((s = br.readLine()) != null) {
+        try (
+            InputStream inputStream = DatabaseConnection.class.getResourceAsStream("/initializeDb.sql");
+            InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(streamReader);) {
+            
+            while((s = br.readLine()) != null){
                 sb.append(s);
             }
             commands = sb.toString().split(";");
-        } catch (Exception e) {
-            e.printStackTrace();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
             System.exit(1);
         }
-
-        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement();) {
+         
+        try (Connection conn = DriverManager.getConnection(DatabaseConnection.URL); Statement stmt = conn.createStatement();) {
 
             DatabaseMetaData meta = connection.getMetaData();
-            System.out.println("the driver naem is " + meta.getDriverName());
+            System.out.println("the driver name is " + meta.getDriverName());
             System.out.println("the database file is " + meta.getURL());
 
             if(commands != null) {
@@ -68,6 +76,7 @@ public class DatabaseConnection {
                     // skip any empty lines found in the file
                     if(!commands[i].trim().equals("")){
                         stmt.executeUpdate(commands[i]);
+                        System.out.println(">>"+commands[i]);
                     }
                 }
             } else {
